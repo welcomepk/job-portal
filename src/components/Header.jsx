42d9a823035639a78d3c7e354d9886916c60b687
pramod kamble <kamblepramod806@gmail.com>
2024-08-31 12:37:22 +0530

    add menu to clerk menuitems

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { BriefcaseBusiness, Heart, PenBox } from "lucide-react";
 import { Link } from 'react-router-dom'
+import { Button } from "./ui/button";
 
 function Header() {
     return (
@@ -12,7 +14,20 @@ function Header() {
                 <SignInButton />
             </SignedOut>
             <SignedIn>
-                <UserButton />
+                <Link to="/post-job">
+                    <Button variant="destructive" className="rounded-full">
+                        <PenBox size={15} className="mr-2" />
+                        Poat a Job
+                    </Button>
+                </Link>
+                <UserButton appearance={{ elements: { avatarBox: "w-10 h-10" } }}>
+                    <UserButton.MenuItems>
+                        <UserButton.Link label="My Jobs" labelIcon={<BriefcaseBusiness size={15} />} href="/my-jobs" />
+                    </UserButton.MenuItems>
+                    <UserButton.MenuItems>
+                        <UserButton.Link label="Saved Jobs" labelIcon={<Heart size={15} />} href="/saved-jobs" />
+                    </UserButton.MenuItems>
+                </UserButton>
             </SignedIn>
         </nav>
     )
